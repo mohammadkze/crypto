2554c8fd576884ec703b6dfe1cf539d250da9856
mohammad <mohammadkhazaie1378@gmail.com>
2025-02-09 23:24:26 +0330

    add category and path

--- app/config/request_generator.py
+++ app/config/request_generator.py
@@ -12,6 +12,17 @@ class APIVersion(Enum):
     V1 = "v1"
     V2 = "v2"
 
+class EndpointCategory(Enum):
+    CRYPTOCURRENCY = "cryptocurrency"
+    EXCHANGE = "exchange"
+    GLOBAL_METRICS = "global-metrics"
+    TOOLS = "tools"
+    BLOCKCHAIN = "blockchain"
+    FIAT = "fiat"
+    PARTNERS = "partners"
+    KEY = "key"
+    CONTENT = "content"
+
 class RequestGenerator:
     def __init__(self, base_url: Optional[str] = None, api_key: Optional[str] = None, 
                  header: Optional[Dict[str, str]] = None) -> None:
@@ -22,16 +33,17 @@ class RequestGenerator:
         self.session: requests.Session = requests.Session()
         self.session.headers.update({
             'Accepts': 'application/json',
+            'Accept-Encoding': 'gzip, deflate', # increase fetching data
             'X-CMC_PRO_API_KEY': self.api_key
         })
 
         if header:
             self.session.headers.update(header)
 
-    def get(self, endpoint: str, version: APIVersion = APIVersion.V1,
+    def get(self, category: EndpointCategory, path: str, version: APIVersion = APIVersion.V1,
             params: Optional[Dict[str, Union[str, int, float]]] = None) -> Dict[str, Any]:
         """Send a GET request to the API with versioning."""
-        url: str = f"{self.base_url}/{version.value}/{endpoint}"
+        url: str = f"{self.base_url}/{version.value}/{category.value}/{path}"
         print(f"url = {url}")
         try:
             response: requests.Response = self.session.get(url, params=params)
@@ -40,10 +52,10 @@ class RequestGenerator:
         except requests.exceptions.RequestException as e:
             return {"error": str(e)}
 
-    def post(self, endpoint: str, version: APIVersion = APIVersion.V1,
+    def post(self, category: str, path: str, version: APIVersion = APIVersion.V1,
              data: Optional[Dict[str, Any]] = None, json_data: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
         """Send a POST request to the API with versioning."""
-        url: str = f"{self.base_url}/{version.value}/{endpoint}"
+        url: str = f"{self.base_url}/{version.value}/{category}/{path}"
         try:
             response: requests.Response = self.session.post(url, data=data, json=json_data)
             response.raise_for_status()
--- app/services/crypto_service.py
+++ app/services/crypto_service.py
@@ -1,20 +1,22 @@
-from app.config.request_generator import RequestGenerator, APIVersion
+from app.config.request_generator import RequestGenerator, APIVersion, EndpointCategory
 from app.models.request.crypto_request import CryptoListRequest
 from app.models.response.crypto_response import CryptoResponse
 from typing import Optional
+import time
 
 class CryptoService(RequestGenerator):
     def __init__(self, base_url: Optional[str] = None, api_key: Optional[str] = None):
         """Inherit from RequestGenerator to handle cryptocurrency-related API requests."""
         super().__init__(base_url, api_key)
 
-    def get_latest_listings(self, request: CryptoListRequest, version: APIVersion = APIVersion.V1) -> CryptoResponse:
+    def get_latest_listings(self, category: EndpointCategory, path: str, request: CryptoListRequest, version: APIVersion = APIVersion.V1) -> CryptoResponse:
         """Fetch the latest cryptocurrency listings with request model."""
-        endpoint = "cryptocurrency/listings/latest"
-        response = self.get(endpoint, version, params=request)
+        start_time = time.time()  # Start timer
+        response = self.get(category, path, version, params=request)
 
         # Convert to Pydantic model
         crypto_response = CryptoResponse(**response)
-
+        end_time = time.time()  # Start time
+        print(f"fetch {end_time - start_time}")
         # Return a dictionary (which is JSON serializable)
         return crypto_response.model_dump()
